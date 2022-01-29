997d34ac4217e5ca0405835eb4d79365e03a114a
NepEvg <eneputina@inbox.ru>
2022-01-29 16:50:50 +0500

    added summa

--- Pr12/Program.cs
+++ Pr12/Program.cs
@@ -6,7 +6,30 @@ namespace Pr12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("введите A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("введите B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("выберите операцию [ + | - | * | / ] ");
+            string ans = Console.ReadLine();
+            switch (ans)
+            {
+                case "+":
+
+                    break;
+                case "-":
+
+                    break;
+                case "*":
+
+                    break;
+                case "/":
+
+                    break;
+                default:
+                    Console.WriteLine("такой операции нет");
+                    break;
+            }
         }
     }
 }
